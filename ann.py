59ee2860178f146495f40cc298f1a43c05c9e597
LiskB <brandenlisk@gmail.com>
2018-08-28 10:16:29 -0400

    Added new way to evaluate the ANN

--- ann.py
+++ ann.py
@@ -33,7 +33,7 @@ X = X[:, 1:]
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
@@ -91,3 +91,29 @@ new_prediction = (new_prediction > 0.5)
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
+
+# Part 4 - Evaluating, Improving and Tuning the ANN
+# IMPORTANT NOTE: ONLY RUN THIS SECTION IF PART 2 AND 3 ARE NOT RUN
+# This is an improved way to build the ANN, but I left the first part in for referral purposes
+
+# Evaluating the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100) 
+accuracies = cross_val_score(estimator = classifier, X = X_train, y= y_train, cv = 10, n_jobs = -1) # input training data, n_jobs = -1 uses ALL CPUS for faster computation
+mean = accuracies.mean()
+variance = accuracies.std()
+
+# Improving the ANN
+
+
+# Tuning the ANN
